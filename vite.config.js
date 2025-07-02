5a1d607b4700a6922e3b12e5c060ba7db4e427b0
Dilith-Zuper <dilith@zuper.co>
2025-07-02 22:50:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // ✅ Important for Netlify to load assets correctly
+
   server: {
     proxy: {
       '/api': {
